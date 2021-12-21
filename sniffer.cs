94b776ff8fee7bd62797599fc6dd7d3656542ff3
Sergio Cabral <git@sergiocabral.com>
2021-12-21 18:05:27 -0300

    Passo 2: Selecionando interface de rede.

--- sniffer.cs
+++ sniffer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace NetworkSniffer
 {
@@ -6,7 +7,37 @@ namespace NetworkSniffer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Network Sniffer... Let's code!");
+            Console.WriteLine("Analisador de Pacote de Rede");
+            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+            var ipAddress = SelectIPAddress();
+            Console.WriteLine("IP: " + ipAddress);
+        }
+
+        static IPAddress SelectIPAddress()
+        {
+            var hostname = Dns.GetHostName();
+            Console.WriteLine("Computador: " + hostname);
+
+            var hostnameEntry = Dns.GetHostEntry(hostname);
+            var ipAddresses = hostnameEntry.AddressList;
+
+            Console.WriteLine("Endereços IP:");
+            for (var i = 0; i < ipAddresses.Length; i++)
+            {
+                var ipAddress = ipAddresses[i];
+                Console.WriteLine("  " + (i + 1) + ") " + ipAddress);
+            }
+
+            int selectedIndex;
+            do
+            {
+                Console.Write("Selecione: ");
+            } while (int.TryParse(Console.ReadLine(), out selectedIndex) == false ||
+                     selectedIndex < 1 ||
+                     selectedIndex > ipAddresses.Length);
+
+            return ipAddresses[selectedIndex - 1];
         }
     }
 }
